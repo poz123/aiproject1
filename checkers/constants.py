b1adc3a2bbb12a62e78617a12be3fe132af316aa
Clementine <rowanshultz@gmail.com>
2023-12-13 11:34:49 -0500

    More in-depth descriptions for "about"

--- checkers/constants.py
+++ checkers/constants.py
@@ -24,11 +24,14 @@ ABOUT_TEXT = '''
 There are three different algorithms that the AI can use, which are described below.
 
 
-1. Random: On its turn the AI creates a list of currently available moves and picks an item from that list at random.
+1. Random:
+On its turn the AI creates a list of currently available moves and picks an item from that list at random.
 
-2. Greedy Best-First Search: On its turn the AI creates a list of currently available moves and picks the move that it thinks has the highest value.
+2. Greedy Best-First Search:
+The Greedy BFS is a local search algorithm, meaning it has no ability to look into the future beyond immediately available moves. It works by assigning values to available moves, and always performing the move with the highest value operating under the greedy assumption that performing the highest value short-term move will lead to an efficient overall game-plan. In this checkers AI, the algorithm prioritizes taking pieces whenever available, then creating a king if possible, and lastly chasing the nearest enemy piece. If there are multiple possible moves available in the highest priority possible it will choose randomly from that priority move-list.
 
-3. Minimax: On its turn the AI evaluates all currently available moves by recursively simulating what would happen if the opponent (you) played perfectly against it afterwards. Based on this evaluation, the AI takes the move it determines to be most likely to gain it a win in the future. 
+3. Minimax:
+Generates a tree that contains all possible moves that are and could be available. The algorithm then assumes itself to be the “maximizer” player, a player that tries to make moves that maximize its chance of winning. It also labels the enemy player as the “minimizer”, who plays to minimize the maximizer’s chance at winning. Next the Minimax will simulate all possible optimal moves for both itself and the minimizer for their respective goals down the tree until reaching a terminal state (win/ loss) or until a specified depth. At this point, it will evaluate the current board, determine how well its score is maximized, and move on to the next simulation. After finding the best possible board state from the simulations, it backtracks and returns the next available move that will lead to the eventuality of a high-value board. This means it always returns the next optimal move. 
  
                     
 The source code for our implementations of these algorithms, as well as for the rest of this project, can be found at github.com/poz123/aiproject1
