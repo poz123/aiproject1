023316aac201f156ba2e64b7df8087c061a5e18e
pfrizz <109550097+pfrizz@users.noreply.github.com>
2023-12-13 09:31:27 -0500

    added a basic description of each of the algorithms to the about page

--- checkers/constants.py
+++ checkers/constants.py
@@ -20,4 +20,16 @@ GREEDY_BFS = 1
 MINIMAX = 2
 
 # menu text
-ABOUT_TEXT = 'Info about our AI goes here'
+ABOUT_TEXT = '''
+There are three different algorithms that the AI can use, which are described below.
+
+
+1. Random: On its turn the AI creates a list of currently available moves and picks an item from that list at random.
+
+2. Greedy Best-First Search: On its turn the AI creates a list of currently available moves and picks the move that it thinks has the highest value.
+
+3. Minimax: On its turn the AI evaluates all currently available moves by recursively simulating what would happen if the opponent (you) played perfectly against it afterwards. Based on this evaluation, the AI takes the move it determines to be most likely to gain it a win in the future. 
+ 
+                    
+The source code for our implementations of these algorithms, as well as for the rest of this project, can be found at github.com/poz123/aiproject1
+'''
